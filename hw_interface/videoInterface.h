0e5f68a4f8c1e9b5136d2f46e2a97fbc0691eb83
Christopher Cole <cole14@purdue.edu>
2012-02-11 13:20:37 -0500

    Clean up videoInterface.h a bit.

--- hw_interface/videoInterface.h
+++ hw_interface/videoInterface.h
@@ -15,33 +15,31 @@ struct buffer {
     size_t length;
 };
 
-typedef struct {
-	int width;
-	int height;
-    unsigned char* array;
-} frame;
-
-//Utility functions
-static void errno_exit (const char* s);
-static int xioctl (int fd, int request, void* arg);
-
-//Image functions
+//Public Image functions
+void read_frame (void *frame_dest);
+//Private Image functions
 static void pixel_YUV2RGB(int y, int u, int v, char *r, char *g, char *b);
 static void array_YUYV2RGB(void *Dest, const void *src, int width, int height);
-void read_frame (void *frame_dest);
 
-//Init functions
+//Public Init functions
 void startVideo(const char *dev_name, io_method io);
+//Private Init functions
 static void start_capturing (void);
 static void init_read(unsigned int buffer_size);
 static void init_mmap (void);
 static void init_userp (unsigned int buffer_size);
 static void init_device (void);
 
-//Uninit functions
+//Public Uninit functions
 void stopVideo();
+//Private Uninit functions
 static void uninit_device (void);
 static void stop_capturing (void);
+
+//Private Utility functions
+static void errno_exit (const char* s);
+static int xioctl (int fd, int request, void* arg);
+
 
 #define CLEAR(x) memset (&(x), 0, sizeof (x))
 
