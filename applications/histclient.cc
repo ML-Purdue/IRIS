0449e4adba5f2fc409e6ef3ff6c2fe837d3ce945
Chris Wendt <chrismwendt@gmail.com>
2012-02-18 15:41:02 -0500

    Tied in the histogram to the tracker program.  Messed with makefiles a lot.

---
+++ applications/histclient.cc
@@ -0,0 +1,123 @@
+#include <SDL/SDL.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include "histogram.h"
+
+#define WIDTH 420
+#define HEIGHT 500
+#define BPP 4
+#define DEPTH 32
+
+#define HIST_W 100
+
+#define HI_THRESH 140
+#define LOW_THRESH 50
+
+SDL_Surface * screen = NULL;
+SDL_Surface * image = NULL;
+
+
+
+int init () {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0 )
+    {
+        return -1;
+    }
+
+    if (!(screen = SDL_SetVideoMode(WIDTH, HEIGHT, DEPTH, SDL_HWSURFACE)))
+    {
+        SDL_Quit();
+        return -1;
+    }
+    SDL_WM_SetCaption ("Histogram", NULL);
+
+    return 0;
+}
+
+
+SDL_Surface *load_image (const char * filename) {
+    //Temporary storage for the image that's loaded
+    SDL_Surface* loadedImage = NULL;
+    //The optimized image that will be used
+    SDL_Surface* optimizedImage = NULL;
+
+    loadedImage = SDL_LoadBMP(filename);
+
+    //If nothing went wrong in loading the image
+    if( loadedImage != NULL ) {
+        //Create an optimized image
+        optimizedImage = SDL_DisplayFormat( loadedImage );
+        //Free the old image
+        SDL_FreeSurface( loadedImage );
+    }
+
+    return optimizedImage;
+
+}
+
+int
+load_files()
+{
+    image = load_image("cropc1dan.bmp");
+    if (image == NULL) {
+        return -1;
+    }
+    return 0;
+}
+
+void clean_up () {
+    SDL_FreeSurface(image);
+
+    SDL_Quit();
+}
+
+int
+main (int argc, char * argv[])
+{
+    SDL_Event event;
+
+    int keypress = 0;
+    int h=0;
+
+    if ( init() < 0 ) {
+        fprintf( stderr, "Could not init.\n");
+        return 1;
+    }
+
+
+    if ( load_files() < 0 ) {
+        fprintf( stderr, "Could not load files.\n");
+        fprintf( stderr, "%s\n", SDL_GetError() );
+        return 1;
+    }
+
+
+    apply_surface(HIST_W, 0, image, screen);
+
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+    draw_hist(image, screen);
+    gettimeofday(&end, NULL);
+
+    long seconds  = end.tv_sec  - start.tv_sec;
+    long useconds = end.tv_usec - start.tv_usec;
+
+    long mtime = ((seconds) * 1000 + useconds/1000.0) + 0.5;
+
+
+
+    printf("time: %li\n", mtime);
+
+    SDL_Flip(screen);
+
+    while (true) {
+
+        if (poll_keypress() == 1){
+            clean_up();
+            return 0;
+        }
+    }
+
+
+}
---
+++ hw_interface/tracker.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+//For SDL screen display
+#include <SDL/SDL.h>
+#include "videoTest.h"
+
+#include "../applications/histogram.h"
+
+#define WIDTH 640
+#define HEIGHT 480
+#define H_WIDTH 100
+
+void drawCrosshair(SDL_Surface *image, int w, int h, int px, int py) {
+	int *p = (int *)image->pixels;
+	for (int x = px - 5; x > 0 && x < w && x < px + 5; x++) {
+		p[py * w + x] = image->format->Gmask;
+	}
+
+	for (int y = py - 5; y > 0 && y < h && y < py + 5; y++) {
+		p[y * w + px] = image->format->Gmask;
+	}
+}
+
+void process_image(SDL_Surface *image, int w, int h) {
+	int darkestSum = 1000000;
+	int dx = 0, dy = 0;
+
+	int *p = (int *)image->pixels;
+	for (int y = 0; y < h; y++) {
+		for (int x = 0; x < w; x++) {
+			int sum = 0;
+			sum += p[y * w + x] & image->format->Rmask;
+			sum += p[y * w + x] & image->format->Gmask;
+			sum += p[y * w + x] & image->format->Bmask;
+
+			if (sum < darkestSum) {
+				darkestSum = sum;
+				dx = x;
+				dy = y;
+			}
+		}
+	}
+
+	drawCrosshair(image, w, h, dx, dy);
+}
+
+int main () {
+    //Init screen
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Surface *screen = SDL_SetVideoMode(WIDTH + H_WIDTH, HEIGHT + H_WIDTH, 32, SDL_SWSURFACE);
+	SDL_Surface *buffer = SDL_CreateRGBSurface(SDL_SWSURFACE, WIDTH, HEIGHT, 32, 0, 0, 0, 0);
+
+    //Init frame grabbing
+    startVideo("/dev/video1");
+
+    //Grab frames and put them on the screen
+    SDL_Event event;
+    do {
+		SDL_FillRect(screen, NULL, SDL_MapRGB( screen->format, 0, 0, 0 ));
+        read_frame(buffer->pixels);
+		process_image(buffer, 640, 480);
+		apply_surface(H_WIDTH, 0, buffer, screen);
+
+		draw_hist(buffer, screen);
+
+        SDL_Flip(screen);
+        SDL_PollEvent(&event);
+    } while (event.type != SDL_QUIT && event.type != SDL_KEYDOWN);
+
+    //Stop grabbing frames
+    stopVideo();
+
+    return 0;
+}
